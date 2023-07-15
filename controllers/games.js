3478dd4c8062028fac5f5696a469a1f81bfc5645
zeusjiaotianci <zeusjiaotianci@gmail.com>
2023-07-14 19:59:03 -0700

    routes and ctrl

--- controllers/games.js
+++ controllers/games.js
@@ -1,8 +1,5 @@
-const Game = require('../models/game');
 const fetch = require('node-fetch');
 const apiKey = 'ec348de508f5456dbbde24cb3729bbb9';
-const sanitizeHtml = require('sanitize-html');
-const striptags = require('striptags');
 const Mylist = require('../models/mylist');
 
 
@@ -10,11 +7,41 @@ const Mylist = require('../models/mylist');
 module.exports = {
     index,
     show,
-    update
+    update,
+    shuffleArray,
 }
 
-async function index(req, res) {
-    res.render('games/index', { title: 'Top games'});
+async function index(req, res, next) {
+    try {
+        const response = await fetch(
+          `https://api.rawg.io/api/games?key=${apiKey}&ordering=-rating&page_size=100&platforms=4&dates=2023-01-01,2023-12-31`
+        );
+        const data = await response.json();
+        const topGames = data.results
+          .map(game => ({
+            id: game.id,
+            name: game.name,
+            coverImageUrl: game.background_image,
+            rating: game.rating,
+          }))
+          .filter(game => game.coverImageUrl !== null);
+    
+      
+        const shuffledGames = shuffleArray(topGames);
+        const randomGames = shuffledGames.slice(0, 40);
+        res.render('games/index', { title: 'Top Games', topGames: randomGames });
+      } catch (error) {
+        console.error(error);
+        res.render('error', { message: 'Failed to fetch top games' });
+      }
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
   }
 
 
--- routes/games.js
+++ routes/games.js
@@ -1,46 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const fetch = require('node-fetch');
-const apiKey = 'ec348de508f5456dbbde24cb3729bbb9';
 const gamesCtrl = require('../controllers/games');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const response = await fetch(
-      `https://api.rawg.io/api/games?key=${apiKey}&ordering=-rating&page_size=100&platforms=4&dates=2023-01-01,2023-12-31`
-    );
-    const data = await response.json();
-    const topGames = data.results
-      .map(game => ({
-        id: game.id,
-        name: game.name,
-        coverImageUrl: game.background_image,
-        rating: game.rating,
-      }))
-      .filter(game => game.coverImageUrl !== null);
-
+router.get('/', gamesCtrl.index);
   
-    const shuffledGames = shuffleArray(topGames);
-    const randomGames = shuffledGames.slice(0, 40);
-    res.render('games/index', { title: 'Top Games', topGames: randomGames });
-  } catch (error) {
-    console.error(error);
-    res.render('error', { message: 'Failed to fetch top games' });
-  }
-});
 router.put('/:id/purchase', gamesCtrl.update);
 
 router.get('/:id', gamesCtrl.show);
 
 
 
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
+
 
 
 module.exports = router;
